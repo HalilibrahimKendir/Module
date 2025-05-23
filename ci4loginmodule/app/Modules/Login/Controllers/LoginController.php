1d2ca513aa811f4be81e698c76fae88822021f21
HalilibrahimKendir <75073970+HalilibrahimKendir@users.noreply.github.com>
2025-05-23 11:16:26 +0300

    Add files via upload

---
+++ ci4loginmodule/app/Modules/Login/Controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Modules\Login\Controllers;
+
+use App\Controllers\BaseController;
+use App\Modules\Login\Models\UserModel;
+
+class LoginController extends BaseController
+{
+    public function index()
+    {
+        return view('App\\Modules\\Login\\Views\\login');
+    }
+
+    public function authenticate()
+    {
+        $session = session();
+        $model = new UserModel();
+        $username = $this->request->getVar('username');
+        $password = $this->request->getVar('password');
+        $data = $model->where('username', $username)->first();
+
+        if ($data) {
+            $pass = $data['password'];
+            $verify_pass = password_verify($password, $pass);
+            if ($verify_pass) {
+                $ses_data = [
+                    'id'       => $data['id'],
+                    'username' => $data['username'],
+                    'email'    => $data['email'],
+                    'logged_in' => TRUE
+                ];
+                $session->set($ses_data);
+                return redirect()->to('/dashboard');
+            } else {
+                $session->setFlashdata('msg', 'Wrong Password');
+                return redirect()->to('/login');
+            }
+        } else {
+            $session->setFlashdata('msg', 'Username not Found');
+            return redirect()->to('/login');
+        }
+    }
+
+    public function logout()
+    {
+        $session = session();
+        $session->destroy();
+        return redirect()->to('/login');
+    }
+} 
